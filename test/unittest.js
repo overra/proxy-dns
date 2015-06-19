90d6c7006a9ac3c917775d7e8a43f6dbcd8301fd
jamesmr89 <james@northshoresoftware.com>
2015-06-19 13:15:24 -0700

    Adding a quick unittest file to verify testing
    Working on getting Travis-CI up and running

---
+++ test/unittest.js
@@ -0,0 +1,11 @@
+var app = require('../index.js');
+var expect = require('expect.js');
+
+describe("Sample Test", function () {
+  describe("I don't know", function () {
+     it('should return blah', function (done) {
+          console.log(app);
+          done();
+     });
+   });
+});
