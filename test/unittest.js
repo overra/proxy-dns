45dffad728de549a63dd1d7c3ec5349f50f4c3a7
Adam Snodgrass <adam@virtualmed.org>
2015-08-04 10:53:39 -0500

    Add first test

--- test/unittest.js
+++ test/unittest.js
@@ -1,12 +1,39 @@
-var app = require('../index.js');
+var dgram = require('dgram')
+var ProxyDNS = require('../index.js');
 var expect = require('expect.js');
 
 // Begin Unit Tests
-describe("Sample Test", function () {
-  describe("I don't know", function () {
-     it('should return blah', function (done) {
-          console.log(app);
-          done();
+describe("ProxyDNS", function () {
+  var app = ProxyDNS()
+
+  function* middleware(next) {}
+
+  describe("#use", function () {
+    app.use(middleware)
+      it('should append a middleware function', function () {
+        expect(app.middleware.slice(-1)[0]).to.be(middleware)
+      });
+   });
+
+   describe("#callback", function () {
+     it('should return a request callback function', function () {
+       expect(app.callback()).to.be.a('function')
+     })
+   })
+
+   describe("#listen", function () {
+     var server;
+
+     it('should change the bind state to 1', function () {
+       server = app.listen(5353);
+       expect(server._socket).to.be.a(dgram.Socket)
+       expect(server._socket._bindState).to.be(1);
      });
+
+     it('should bind request event callback', function () {
+       expect(server._events.request.toString()).to.be(ProxyDNS.prototype.callback().toString())
+     });
+
+
    });
 });
