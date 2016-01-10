c42db187bae2263e6e46158126982bed468448b7
Adam Snodgrass <adam@virtualmed.org>
2016-01-10 15:15:03 -0600

    Change source to take advantage of es2015 features

--- src/context.js
+++ src/context.js
@@ -1,76 +1,77 @@
 'use strict';
 
-let dns = require('native-dns');
-let defaults = require('lodash.defaults');
+import dns, {consts} from 'native-dns';
 
-var context = module.exports = {
+export default {
   type: 'dns',
   retries: 0,
   maxRetries: 3,
-  onerror: function(err) {
-    console.error(err.stack);
-    this.res.header.rcode = dns.consts.NAME_TO_RCODE.SERVFAIL;
-    this.res.send();
-  },
-  resolve: function () {
-    let self = this;
-    let config = self.app.config;
-    let timedOut = false;
-    let q = self.req.question[0];
-    let servers = config.servers;
-    let serverDefaults = {port: 53, type: 'udp'};
-    let server;
-
-    if (Array.isArray(servers)) {
-      server = servers[self.retries] || servers[self.retries % servers.length];
-    } else {
-      server = servers;
-    }
-
-    server = defaults(server, serverDefaults);
-
-    let question = new dns.Question({
-      name: q.name,
-      type: dns.consts.QTYPE_TO_NAME[q.type]
-    });
-
-    let request = dns.Request({
-      question: question,
-      server: server,
-      timeout: config.timeout
-    });
-
-    request.on('timeout', function () {
-      timedOut = true;
-    });
-
-    request.on('message', function (err, response) {
-      response.answer.forEach(function (answer) {
-        self.res.answer.push(answer);
-      });
-    });
-
-    request.on('end', function () {
-      if (!self.res.answer.length) {
-        self.res.header.rcode = dns.consts.NAME_TO_RCODE.NOTFOUND;
-      } else {
-        self.res.header.rcode = dns.consts.NAME_TO_RCODE.NOERROR;
-      }
-
-      if (!timedOut) {
-        self.res.send();
-      } else if (self.retries < self.maxRetries - 1) {
-        self.retries++;
-        self.resolve();
-      }
-    });
-
-    request.send();
-  },
-  answer: function (type, answer) {
-    var answers = this.res.answer;
-    if (typeof dns[type] === 'function') {
-      answers.push(dns[type](answer));
-    }
-  }
+  answer,
+  onerror,
+  resolve
 };
+
+function answer(type, answer) {
+  if (typeof dns[type] === 'function') {
+    this.res.answer = [...this.res.answer, dns[type](answer)];
+  }
+}
+
+function onerror(err) {
+  console.error(err.stack);
+  this.res.header.rcode = consts.NAME_TO_RCODE.SERVFAIL;
+  this.res.send();
+}
+
+function resolve() {
+  const serverDefaults = {port: 53, type: 'udp'};
+  const config = this.app.config;
+  const q = this.req.question[0];
+  const servers = config.servers;
+  const res = this.res;
+  let timedOut = false;
+  let server = servers;
+
+  if (Array.isArray(servers)) {
+    server = servers[this.retries] || servers[this.retries % servers.length];
+  }
+
+  server = {...serverDefaults, ...server};
+
+  let question = new dns.Question({
+    name: q.name,
+    type: consts.QTYPE_TO_NAME[q.type]
+  });
+
+  let request = dns.Request({
+    question: question,
+    server: server,
+    timeout: config.timeout
+  });
+
+  const timeoutHandler = () => timedOut = true;
+  const messageHandler = (err, response) => {
+    response.answer.forEach(answer => {
+      res.answer = [...res.answer, answer];
+    });
+  };
+  const endHandler = () => {
+    if (!this.res.answer.length) {
+      res.header.rcode = consts.NAME_TO_RCODE.NOTFOUND;
+    } else {
+      res.header.rcode = consts.NAME_TO_RCODE.NOERROR;
+    }
+
+    if (!timedOut) {
+      res.send();
+    } else if (this.retries < this.maxRetries - 1) {
+      this.retries++;
+      this.resolve();
+    }
+  };
+
+  request.on('timeout', timeoutHandler);
+  request.on('message', messageHandler);
+  request.on('end', endHandler);
+  request.send();
+}
--- src/index.js
+++ src/index.js
@@ -1,64 +1,57 @@
 'use strict';
 
-let dns = require('native-dns');
-let compose = require('koa-compose');
-let co = require('co');
-let context = require('./context');
-let defaults = require('lodash.defaults')
+import dns from 'native-dns';
+import compose from 'koa-compose';
+import co from 'co';
+import context from './context';
 
-let app = ProxyDNS.prototype;
+export default class ProxyDNS {
+  constructor(config={}) {
+    let defaultConfig = {
+      servers: [
+        {address: '8.8.4.4'},
+        {address: '8.8.8.8'}
+      ],
+      timeout: 250
+    };
 
-exports = module.exports = ProxyDNS;
-
-function ProxyDNS(config) {
-  if (!(this instanceof ProxyDNS)) return new ProxyDNS(config);
-
-  let defaultConfig = {
-    servers: [
-      {address: '8.8.4.4'},
-      {address: '8.8.8.8'}
-    ],
-    timeout: 250
-  };
-  config = config || {};
-
-  this.config = defaults(config, defaultConfig);
-  this.middleware = [];
-  this.context = Object.create(context);
-}
-
-app.use = function(fn) {
-  this.middleware.push(fn);
-  return this;
-};
-
-app.listen = function() {
-  let server = dns.createServer();
-  server.on('request', this.callback());
-  server.serve.apply(server, arguments);
-  return server;
-};
-
-app.callback = function() {
-  let self = this;
-  let middleware = [respond].concat(this.middleware);
-  let fn = co.wrap(compose(middleware));
-
-  return function(req, res) {
-    let ctx = self.createContext(req, res);
-    fn.call(ctx).catch(ctx.onerror);
+    this.config = {...defaultConfig, ...config};
+    this.context = Object.create(context);
+    this.middleware = [];
   }
-};
 
-app.createContext = function(req, res) {
-  let ctx = Object.create(this.context);
-  ctx.app = this;
-  ctx.req = req;
-  ctx.req.ip = req.address.address;
-  ctx.res = res;
-  ctx.onerror = ctx.onerror.bind(ctx);
-  return ctx;
-};
+  use(fn) {
+    this.middleware.push(fn);
+    return this;
+  }
+
+  listen() {
+    let server = dns.createServer();
+    server.on('request', this.requestHandler());
+    server.serve.apply(server, arguments);
+    return server;
+  }
+
+  requestHandler() {
+    let middleware = [respond, ...this.middleware];
+    let fn = co.wrap(compose(middleware));
+
+    return (req, res) => {
+      let ctx = this.createContext(req, res);
+      fn.call(ctx).catch(ctx.onerror);
+    }
+  }
+
+  createContext(req, res) {
+    let ctx = Object.create(this.context);
+    ctx.app = this;
+    ctx.req = req;
+    ctx.req.ip = req.address.address;
+    ctx.res = res;
+    ctx.onerror = ::ctx.onerror;
+    return ctx;
+  }
+}
 
 function *respond(next) {
   yield *next;
